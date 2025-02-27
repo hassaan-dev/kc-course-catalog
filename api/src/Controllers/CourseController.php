3a2069d14342128b16b2a58475f7d63dafc9d820
Muhammad Hassaan <hassaan.developer@yahoo.com>
2025-02-28 01:22:11 +0500

    refactor(core): add return types, type hints, and documentation

--- api/src/Controllers/CourseController.php
+++ api/src/Controllers/CourseController.php
@@ -5,11 +5,21 @@ namespace App\Controllers;
 use App\Models\Course;
 
 class CourseController {
-    public static function getAllCourses($category_id = null) {
+    /**
+     * @param $category_id
+     * @return array
+     */
+    public static function getAllCourses($category_id = null): array
+    {
         return Course::getAll($category_id);
     }
 
-    public static function getCourseById($id) {
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public static function getCourseById($id): mixed
+    {
         return Course::getById($id);
     }
 }
